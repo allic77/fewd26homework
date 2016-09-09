d11df727580db414b61bf130fbec95337d3232c5
allic77 <allisoncrompton@Allisons-MacBook-Air.local>
2016-09-08 21:03:45 -0400

    week05homework

---
+++ week05/Assignment/homework/js/index.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function(){
+	
+// When the Read More is clicked display the show-this-on-click. //
+    jQuery('.readmore').click(function){
+        e.preventDefault();
+        jQuery('#show-this-on-click').slideDown();
+        jQuery('p.readmore').hide();
+        jQuery('p.readless hide').show();
+    });
+
+// When the Read Less is clicked slide back up. //
+    jQuery('.readless').click(function){
+    	e.preventDefault();
+    	jQuery('#show-this-on-click').slideUp();
+    	jQuery('p.readmore').show();
+    	jQuery('p.readless hide').hide();
+    });
+
+// When the Learn More is clicked display the learnmoretext. //
+    jQuery('.learnmore').click(function){
+    	e.preventDefault();
+    	jQuery('#learmoretext').slideDown();
+    	jQuery('a.learnmore').show();
+    	jQuery('a.learnmore').hide();
+    });
+});
