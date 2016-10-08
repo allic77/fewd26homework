92aace581706476149ebd7912d3a5ae1b9607c7a
allic77 <allisoncrompton@Allisons-MacBook-Air.local>
2016-10-08 16:08:03 -0400

    Allison Crompton final project

---
+++ final project/alli site 2/js/alli.js
@@ -0,0 +1,14 @@
+// jQuery(document).ready(function(){
+//     jQuery('#relaxrExistingUser a').click(function (e) {
+//         e.preventDefault()
+//         jQuery(this).tab('show')
+//     })
+// });
+
+
+jQuery(document).ready(function(){
+	event.preventDefault();
+	})
+});
+
+
